edd302a6f63e398adb5a8f1d5f3202d05dc6b04a
Rishav Mehra <rishavmehra61@gmail.com>
2022-06-17 17:24:04 +0530

    changes

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -108,7 +108,7 @@ while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	frame = imutils.resize(frame, width=1000)
 
 	# detect faces in the frame and determine if they are wearing a
 	# face mask or not
